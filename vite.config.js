df5d6422a9efb915714194df54b0c2fc41b0c811
BrunoCavaca <bruno.fonseca_cavaca@kcl.ac.uk>
2025-09-09 11:02:28 +0100

    tiny fix to url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal_website/",
+  base: "/personal_website",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
