2cf9d8c3c0989fe7b760fdbd6d60ae0010830972
BrunoCavaca <bruno.fonseca_cavaca@kcl.ac.uk>
2025-09-09 10:59:42 +0100

    fix url pointers

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bruno.github.io/",
+  base: "/personal_website/",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
